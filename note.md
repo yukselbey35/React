447105a47895263c8e53088558ca626574646dfa
yuksel <yukselbey35@gmail.com>
2021-03-20 13:51:28 -0700

    First intro file

---
+++ note.md
@@ -0,0 +1,4 @@
+1st crate react app : npm install -g create-react-app
+2nd Create  'intro' folder : npx create-react-app intro
+3rd create componenets Navi.js in src
+4th type 'rcc' to create a class companent in the Navi.js
---
+++ src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Navigation Component</h1>
+            </div>
+        )
+    }
+}
